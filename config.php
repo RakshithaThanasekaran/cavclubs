1af9d9b1bd8d35afad047fd60b27f8ba41348ee0
Shriya Dale <144620275+ShriyaDale@users.noreply.github.com>
2025-11-09 13:42:32 -0500

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'LOCAL_IP_HOST' => '127.0.0.1',
-    'CLOUD_SQL_HOST' => 'cs4750db-476016:us-east4:cs4750',
-    'DB_NAME' => 'CavClubs',
-    'DB_USER' => 'root',
-    'DB_PASSWORD' => 'dBpro0ject4750'
-];
